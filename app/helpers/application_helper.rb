014491819e489820561279fd2b6341f0ac648db6
Michael Washburn <washburn.michael@gmail.com>
2017-12-22 13:23:49 -0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #This is a custom helper. It returns the full title on a per-page basis. This is like writing a function in Javascript
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App" #variable holding string to be placed in title
+    if page_title.empty?  #conditional
+      base_title  #return just base_title if a page_title is not present
+    else
+      page_title + " | " + base_title  #otherwise, reture page_title and base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get "/"
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   # test "should get home" do
